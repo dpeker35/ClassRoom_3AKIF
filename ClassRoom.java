471a820af2264f0f839edfd13193c4cedc9eff8a
Didem Peker <pek240759@spengergasse.at>
2024-09-26 11:49:24 +0200

    Initial sharing of project

---
+++ ClassRoom.java
@@ -0,0 +1,59 @@
+public class ClassRoom
+{
+  private String roomNr;
+  private boolean computer;
+  private int       size;
+  
+  
+  public ClassRoom(String newRoomNr, boolean newComputer, int newSize)
+  {
+      setRoomNr(newRoomNr);
+      setComputer(newComputer);
+      setSize(newSize);
+  }
+  
+  
+   public void setRoomNr(String newRoomNr)
+  {
+      roomNr = newRoomNr;
+  }
+  
+  public void setComputer(boolean newComputer)
+  {
+      computer = newComputer;
+  }
+  
+  public void setSize(int newSize)
+  {
+      size = newSize;
+  }
+  
+  
+  
+  public String getRoomNr()
+  {
+      return roomNr;
+  }
+  
+  public boolean getComputer()
+  {
+      return computer;
+  }
+  
+  public int getSize()
+  {
+      return size;
+  }
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
